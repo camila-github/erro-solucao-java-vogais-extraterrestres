11d9d5c7f3e0fb31e2b3ea4e688f8146cc7ad2a8
Camila Augusta dos Santos <camilaaugustasantos@yahoo.com.br>
2021-07-01 16:14:41 -0300

    VogaisExtraterrestres

---
+++ java-vogais-extraterrestres.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class VogaisExtraterrestres {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        String alfabeto, frase;
+        int quantidade = 0;
+        
+        while (scr.hasNextLine()) {
+            quantidade = 0;
+            alfabeto = scr.nextLine();
+            frase = scr.nextLine();
+    
+            for (int i = 0; i < alfabeto.length() ; i++) {
+                for (int j = 0; j < frase.length(); j++) {
+                    if (frase.charAt(j) == alfabeto.charAt(i)) quantidade++;
+                }
+            }
+            System.out.println(quantidade);
+        }
+    }
+}
